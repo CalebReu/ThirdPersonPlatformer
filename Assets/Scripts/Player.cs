67024231c235bc18b1ddbb8da3abfe8c1fcb1b05
CalebReu <caleb.reurink@gmail.com>
2025-02-19 17:50:12 -0800

    tuned player movement
    - added max speed
    - added air control modifier

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour
     [SerializeField] private InputManager inputManager;
     [SerializeField] private CinemachineCamera playerCamera;
     [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [Tooltip("How much the player can control their movement in the air as a fraction of their ground control.")]
+    [SerializeField] private float airControlStrength;
     [SerializeField] private float jumpStrength;
 
     private Rigidbody rb;
@@ -24,10 +27,33 @@ public class Player : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y);
         moveDirection = transform.TransformDirection(moveDirection);
-        rb.AddForce(speed * moveDirection);
+        // if moving at max speed, set the component of moveDirection in the direction of movement to zero
+        Vector3 velocity = rb.linearVelocity;
+        velocity.y = 0f;
+        if (velocity.magnitude > maxSpeed && Vector3.Dot(velocity, moveDirection) > 0f)
+        {
+            Vector3 velocityDirection = velocity.normalized;
+            moveDirection -= Vector3.Project(moveDirection, velocityDirection);
+        }
+
+        if (IsGrounded())
+        {
+            rb.AddForce(speed * moveDirection.normalized);
+        }
+        else
+        {
+            rb.AddForce(speed * airControlStrength * moveDirection.normalized);
+        }
     }
     private void Jump()
     {
-        rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+        if (IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.3f);
     }
 }
